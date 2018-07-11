e8b0e9b2bb28e344fb4ef7cc6526186a96e45064
kamlin <424494431@qq.com>
2018-07-11 13:52:50 +0800

    添加allocator笔记和代码

--- 12/MemoryTest.cpp
+++ 12/MemoryTest.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <memory>
 #include <list>
+#include <vector>
 
 using namespace std;
 
@@ -26,5 +27,16 @@ int main() {
     u2.release(); //放弃u2对指针的控制权， 返回对应的指针， 并将u2置空
     u2.reset(); //释放u指向的对象
     u2.reset(new string()); //如果传入的是内置指针， 将u2指向改对象， 否则将u2置空
+
+    allocator<string> a; //定义了一个名为a的allocator对象， 他可以为string对象分配内存
+    string *sp = a.allocate(10); //分配10个string对象的内存
+    a.deallocate(sp, 10); //释放从sp指针中地址开始的内存， 这块内存保存了10个对象 sp必须是先前allocate方法返回的指针。在调用deallocate之前， 必须调用对象的destroy
+    a.construct(sp, "s"); //在sp指向的内存中构造一个对象
+    a.destroy(sp); //执行对象的析构函数
+
+    vector<int> v;
+    auto ap = a.allocate(v.size() * 2);
+    auto aq = uninitialized_copy(v.begin(), v.end(), ap); //返回的地址是最后一个指针
+    uninitialized_fill_n(aq, v.size(), 42);
 
 }
